115c3365a95a8d953fba07912ea77d321c418878
Jon Calvert <55768542+JonC-Ice92@users.noreply.github.com>
2019-10-29 17:45:36 -0700

    class

---
+++ prossessing/first_scetch/sketch.js
@@ -0,0 +1,24 @@
+var x = [];
+var speed = .5
+function setup() {
+  createCanvas(2000, 800);
+  noStroke();
+  fill(255, 200);
+  for (var i = 0; i < 3000; i++) {
+    x[i] = random(-1000, 200);
+  }
+}
+
+function draw() {
+  
+  background(50);
+  for (var i = 0; i < x.length; i++) {
+    x[i] += 5;
+    var y = i * 0.4;
+    arc(x[i], y, 12, 12, 0.52, 5.76);
+    stroke(random(0,255))
+    fill(random(0,255))
+    
+  }
+  
+}
