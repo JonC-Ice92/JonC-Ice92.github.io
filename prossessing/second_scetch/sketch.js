d1847b90e0d73d914b426a768c016ca868895006
JonC-Ice92 <55768542+JonC-Ice92@users.noreply.github.com>
2019-11-07 12:36:29 -0800

    Giving credit where it is due

--- prossessing/second_scetch/sketch.js
+++ prossessing/second_scetch/sketch.js
@@ -1,7 +1,20 @@
+  
+var x = [];
+
 function setup() {
-  // put setup code here
+  createCanvas(240, 120);
+  noStroke();
+  fill(255, 200);
+  for (var i = 0; i < 3000; i++) {
+    x[i] = random(-1000, 200);
+  }
 }
 
 function draw() {
-  // put drawing code here
+  background(0);
+  for (var i = 0; i < x.length; i++) {
+    x[i] += 0.5;
+    var y = i * 0.4;
+    arc(x[i], y, 12, 12, 0.52, 5.76);
+  }
 }
